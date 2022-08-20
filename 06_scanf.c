261b2738fa058705cda404155620bbb645411d8f
husita-h <taisei.fukushi1129@gmail.com>
2022-08-20 17:41:04 +0900

    05 scanf関数 キーボードからの入力を受け付ける

---
+++ 06_scanf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// int main (void) {
+//     int number;
+//     scanf("%d\n", &number);
+//     printf("入力された値は%d\n", number);
+//     return 0;
+// }
+
+int main (void) {
+    double number;
+    scanf("%lf\n", &number);
+    printf("入力された値は%lfです\n", number);
+    return 0;
+}
